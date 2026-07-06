e28da36c12dd90217ef610fc3b64064630c4d201
dahlah <zikribukhari06@gmail.com>
2026-07-06 20:01:31 +0700

    Update mpu6050.ino

--- mpu6050.ino
+++ mpu6050.ino
@@ -11,14 +11,17 @@ void setup () {
 
   byte status = mpu.begin();
   Serial.print("status mpu 6050 : ");
-  Serial.println(status);
+  Serial.println(status); // status menjunjukan kondisi error dari mpu, maka akan benilai 1 ketika error dan 0 ketika berhasil di inisialisasi
 
-  while(status != 0) {}
+  while(status != 0) {
+    Serial.println("MPU gagal diinisialisasikan.....");
+    delay(1000);
+  } // jika status bernilai 1 maka program akan mengalami loop sampai program status bernilai 0
 
   Serial.print("Menghitung gyro offset, jangan gerakkan mpu6050");
   delay(1000);
-
-  mpu.calcOffsets(true, true);
+  
+  mpu.calcOffsets(true, true); //(gyro, acc), nilai true dan false menunujukan apakah keduanya ingin dikalibrasi atau tidak, true jika iya.
   Serial.println("kalibrasi selesai ");
 }
 
